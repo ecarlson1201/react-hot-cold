070ddfc61a3f51348abef99183177c6b3db0a59b
Eric Carlson <eric.paul.carlson@gmail.com>
2019-02-11 03:08:49 -0800

    reconnected with redux

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './feedback.css';
 
-export default function Feedback(props) {
+export function Feedback(props) {
   /** 
    * Below, we'll use the guessCount to generate a key so that React treats the feedback message 
    * as a DOM change, even when a guess does not change the feedback text.
    * This is necessary for consistent aural feedback via aria-live.
   */
-  const key = props.guessCount;
+  const key = props.guessCount
 
   let guessAgain;
   if (key !== 0) {
     guessAgain = <span className="visuallyhidden">Guess again!</span>;
   }
   return (
-    <h2 
+    <h2
       key={key}
       id="feedback"
       role="status"
@@ -26,3 +27,10 @@ export default function Feedback(props) {
     </h2>
   );
 }
+
+const mapPropsToState = state => ({
+  guessCount: state.guesses.length,
+  feedback: state.feedback
+});
+
+export default connect(mapPropsToState)(Feedback);
